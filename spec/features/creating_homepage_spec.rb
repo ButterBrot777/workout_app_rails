fff6f87c9117b9eadac36f191da2d531aba6b338
ButterBrot777 <korendos@gmail.com>
2022-11-18 15:25:50 +0300

    spec: write home page spec

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,10 @@
+RSpec.feature "Created Home Page" do
+  scenario "has content" do
+    visit "/"
+
+    expect(page).to have_link("Home")
+    expect(page).to have_link("Athletes Den")
+    expect(page).to have_content("Workout Lounge!")
+    expect(page).to have_content("Show off your workout")
+  end
+end
