95ab352ec01b7090b2cfd4e2f3ecd06ed972ff24
ButterBrot777 <korendos@gmail.com>
2022-11-21 15:44:58 +0300

    spec: dashboard page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.backtrace_excludion_patterns = [
+  config.backtrace_exclusion_patterns = [
     %r{/lib\d*/ruby/},
     %r{bin/},
     /gems/,
