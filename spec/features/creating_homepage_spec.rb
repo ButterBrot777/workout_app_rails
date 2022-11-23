6e9c282fa32c46d8662cb581e19522b5bba59ce7
ButterBrot777 <korendos@gmail.com>
2022-11-23 16:34:08 +0300

    spec: add auth tests

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -1,10 +1,12 @@
-RSpec.feature "Created Home Page" do
-  scenario "has content" do
-    visit "/"
+# frozen_string_literal: true
 
-    expect(page).to have_link("Home")
-    expect(page).to have_link("Athletes Den")
-    expect(page).to have_content("Workout Lounge!")
-    expect(page).to have_content("Show off your workout")
+RSpec.feature 'Created Home Page' do
+  scenario 'has content' do
+    visit '/'
+
+    expect(page).to have_link('Home')
+    expect(page).to have_link('Athletes Den')
+    expect(page).to have_content('Workout Lounge!')
+    expect(page).to have_content('Show off your workout')
   end
 end
