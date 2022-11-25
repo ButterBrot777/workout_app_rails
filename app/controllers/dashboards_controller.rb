ef160b20149f19b102acfc61df2caa6d6b8ee71e
ButterBrot777 <korendos@gmail.com>
2022-11-25 16:25:46 +0300

    feat: add user registration features

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DashboardsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index]
 
   def index; end
 end
